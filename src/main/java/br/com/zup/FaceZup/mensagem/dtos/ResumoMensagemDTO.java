460f37a38132a56977eca01e03a73f01dd065a0e
tacilia.almeida <tacilia@icloud.com>
2021-08-30 17:49:53 -0300

    Criar classe ResumoMnesagemDTO

---
+++ src/main/java/br/com/zup/FaceZup/mensagem/dtos/ResumoMensagemDTO.java
@@ -0,0 +1,38 @@
+package br.com.zup.FaceZup.mensagem.dtos;
+
+import java.time.LocalDate;
+
+public class ResumoMensagemDTO {
+
+    private String mensagem;
+    private LocalDate dataDeEnvio;
+    private boolean visualizado;
+
+    public ResumoMensagemDTO(){
+
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDate getDataDeEnvio() {
+        return dataDeEnvio;
+    }
+
+    public void setDataDeEnvio(LocalDate dataDeEnvio) {
+        this.dataDeEnvio = dataDeEnvio;
+    }
+
+    public boolean isVisualizado() {
+        return visualizado;
+    }
+
+    public void setVisualizado(boolean visualizado) {
+        this.visualizado = visualizado;
+    }
+}
