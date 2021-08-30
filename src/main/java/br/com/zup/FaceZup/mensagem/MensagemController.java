f2b9be7fa9638d866ad48848e62d11e85f360bc4
tacilia.almeida <tacilia@icloud.com>
2021-08-30 18:30:12 -0300

    Criar metodo mensagem controller que pesquisa mensagens não lidas, lê mensagem através do IdMensagem

--- src/main/java/br/com/zup/FaceZup/mensagem/MensagemController.java
+++ src/main/java/br/com/zup/FaceZup/mensagem/MensagemController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/mensagens")
 public class MensagemController {
@@ -30,5 +33,11 @@ public class MensagemController {
         Mensagem mensagem = mensagemService.visualizarMensagemPorId(id);
         return modelMapper.map(mensagem, ResumoMensagemDTO.class);
     }
-
+    @GetMapping
+    public List<MensagemIdDTO> pesquisarMensagens(@RequestParam(required = false) String emailUsuario,
+                                                  @RequestParam(required = false) Boolean visualizado){
+        List<Mensagem> mensagens = mensagemService.filtrarMensagem(emailUsuario, visualizado);
+        List<MensagemIdDTO> mensagemIdDTOS = mensagens.stream().map(mensagem -> modelMapper.map(mensagem, MensagemIdDTO.class)).collect(Collectors.toList());
+        return mensagemIdDTOS;
+    }
 }
