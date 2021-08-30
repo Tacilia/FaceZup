adf318d899138b8aa847130b248076a81133b535
tacilia.almeida <tacilia@icloud.com>
2021-08-30 18:11:22 -0300

    Criar classe resumo UsuarioDTO

---
+++ src/main/java/br/com/zup/FaceZup/usuario/dtos/ResumoUsuarioDTO.java
@@ -0,0 +1,52 @@
+package br.com.zup.FaceZup.usuario.dtos;
+
+import br.com.zup.FaceZup.usuario.Usuario;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ResumoUsuarioDTO {
+
+    private String email;
+    private String nome;
+    private String sobreNome;
+
+    public ResumoUsuarioDTO(String emai, String nome, String sobreNome){
+        this.email = emai;
+
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+    public static List<ResumoUsuarioDTO> converterListaDeModelParaDTO(List<Usuario> usuarios){
+        return usuarios.stream()
+                .map(usuario -> new ResumoUsuarioDTO(
+                        usuario.getEmail(),
+                        usuario.getNome(),
+                        usuario.getSobrenome())
+                ).collect(Collectors.toList());
+    }
+
+}
